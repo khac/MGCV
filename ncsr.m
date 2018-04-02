5a48bd4c461d39b7ddd04e3f889bc3a0ed68e6f4
Adit Chawdhary <khac@users.noreply.github.com>
2018-04-02 14:26:48 +0530

    Update ncsr.m

--- ncsr.m
+++ ncsr.m
@@ -1,23 +1,54 @@
-clc;
-clear;
+function [im_out PSNR SSIM]   =  NCSR_Superresolution( par )
+par.step      =   2;
+par.win       =   6;
+par.cls_num   =   64;
+par.s1        =   25;
+par.hp        =   75;
 
-fn                 =    'C:\Users\MAHE\Documents\MATLAB\NCSR_IR\Data\SR_test_images\car.jpg';     
-psf                =    fspecial('gauss', 7, 1.6);
-scale              =    3;
-nSig               =    0;
+s             =   par.scale;
+lr_im         =   par.LR;
+[lh lw ch]    =   size(lr_im);
+hh            =   lh*s;
+hw            =   lw*s;
+hrim          =   uint8(zeros(hh, hw, ch));
+ori_im        =   zeros(hh,hw);
 
-par                =    NCSR_SR_Par( nSig, scale, psf );
-par.I              =    double( imread( fn ) );
-LR                 =    Blur('fwd', par.I, psf);
-LR                 =    LR(1:par.scale:end,1:par.scale:end,:);    
-par.LR             =    Add_noise(LR, nSig);   
-par.B              =    Set_blur_matrix( par );  
-
-[im PSNR SSIM]     =    NCSR_Superresolution( par );
-disp( sprintf('%s :  PSNR = %2.2f  SSIM = %2.4f\n\n', fn, PSNR, SSIM));
-
-if nSig == 0
-    imwrite(im./255, 'C:\Users\MAHE\Documents\MATLAB\Results\SR_results\Noiseless\NCSR_car300x300.jpg');
+if  ch == 3
+    lrim           =   rgb2ycbcr( uint8(lr_im) );
+    lrim           =   double( lrim(:,:,1));    
+    b_im           =   imresize( lr_im, s, 'bicubic');
+    b_im2          =   rgb2ycbcr( uint8(b_im) );
+    hrim(:,:,2)    =   b_im2(:,:,2);
+    hrim(:,:,3)    =   b_im2(:,:,3);
+    if isfield(par, 'I')
+        ori_im         =   rgb2ycbcr( uint8(par.I) );
+        ori_im         =   double( ori_im(:,:,1));
+    end
 else
-    imwrite(im./255, 'Results\SR_results\Noisy\NCSR_car400x400.jpg');
+    lrim           =   lr_im;
+    
+    if isfield(par, 'I')
+        ori_im             =   par.I;
+    end
 end
+hr_im    =   imresize(lrim, s, 'bicubic');
+hr_im    =   Superresolution(lrim, par, ori_im, hr_im, 0, 4);
+hr_im    =   Superresolution(lrim, par, ori_im, hr_im, 1, 2);
+
+if isfield(par,'I')
+   [h w ch]  =  size(par.I);
+   PSNR      =  csnr( hr_im(1:h,1:w), ori_im, 0, 0 );
+   SSIM      =  cal_ssim( hr_im(1:h,1:w), ori_im, 0, 0 );
+end
+if ch==3
+    hrim(:,:,1)  =  uint8(hr_im);
+    im_out       =  double(ycbcr2rgb( hrim ));
+else
+    im_out  =  hr_im;
+end
+return;
+
+
+
+
+
