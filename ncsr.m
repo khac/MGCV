0d37fd09cda1b86bd57ff1795831ba5a3c84ce25
Adit Chawdhary <khac@users.noreply.github.com>
2018-04-02 14:31:14 +0530

    Update ncsr.m

--- ncsr.m
+++ ncsr.m
@@ -1,54 +1,51 @@
-function [im_out PSNR SSIM]   =  NCSR_Superresolution( par )
-par.step      =   2;
-par.win       =   6;
-par.cls_num   =   64;
-par.s1        =   25;
-par.hp        =   75;
+function  hr_im     =   Superresolution(lr_im, par, ori_im, hr_im0, flag, K)
+hr_im      =   imresize( lr_im, par.scale, 'bicubic' );
+[h   w]    =   size(hr_im);
+[h1 w1]    =   size(ori_im);
+y          =   lr_im;
+lamada     =   par.lamada;
 
-s             =   par.scale;
-lr_im         =   par.LR;
-[lh lw ch]    =   size(lr_im);
-hh            =   lh*s;
-hw            =   lw*s;
-hrim          =   uint8(zeros(hh, hw, ch));
-ori_im        =   zeros(hh,hw);
+lam       =   zeros(0);
+gam       =   zeros(0);
+BTY       =   par.B'*y(:);
+BTB       =   par.B'*par.B;
+cnt       =   0;
+if  flag==1  
+     hr_im    =  hr_im0;
+end
 
-if  ch == 3
-    lrim           =   rgb2ycbcr( uint8(lr_im) );
-    lrim           =   double( lrim(:,:,1));    
-    b_im           =   imresize( lr_im, s, 'bicubic');
-    b_im2          =   rgb2ycbcr( uint8(b_im) );
-    hrim(:,:,2)    =   b_im2(:,:,2);
-    hrim(:,:,3)    =   b_im2(:,:,3);
-    if isfield(par, 'I')
-        ori_im         =   rgb2ycbcr( uint8(par.I) );
-        ori_im         =   double( ori_im(:,:,1));
+for k    =  1:K    
+    Dict                 =   KMeans_PCA( hr_im, par, par.cls_num );
+    [blk_arr wei_arr]    =   Block_matching( hr_im, par); 
+
+    if flag==1
+        lam      =   Sparsity_estimation( hr_im, par, Dict, blk_arr, wei_arr );        
     end
-else
-    lrim           =   lr_im;
-    
-    if isfield(par, 'I')
-        ori_im             =   par.I;
+    Reg          =   @(x, y)NCSR_Regularization(x, y, par, Dict, blk_arr, wei_arr, lam, flag );
+    f            =   hr_im;
+    X_m          =   Update_NLM( f, par, blk_arr, wei_arr );
+        
+    for  iter    =   1 : par.iters
+        cnt      =   cnt  +  1;           
+        f_pre    =   f;
+   
+        if (mod(cnt, 40) == 0)
+            if isfield(par,'I')
+                PSNR     =  csnr( f(1:h1,1:w1), ori_im, 0, 0 );
+                fprintf( 'NCSR super-resolution, iter. %d : PSNR = %f\n', cnt, PSNR );
+            end
+        end
+
+        f        =   f_pre(:);
+        for i = 1:par.n
+            f    =   f + lamada.*(BTY - BTB*f);
+        end
+        
+        if ( mod(iter, 2)==0 )
+            X_m    =  Update_NLM( reshape(f, h,w), par, blk_arr, wei_arr );
+        end                
+        
+        f        =  Reg( reshape(f, h,w), X_m );        
     end
+    hr_im   =  f;
 end
-hr_im    =   imresize(lrim, s, 'bicubic');
-hr_im    =   Superresolution(lrim, par, ori_im, hr_im, 0, 4);
-hr_im    =   Superresolution(lrim, par, ori_im, hr_im, 1, 2);
-
-if isfield(par,'I')
-   [h w ch]  =  size(par.I);
-   PSNR      =  csnr( hr_im(1:h,1:w), ori_im, 0, 0 );
-   SSIM      =  cal_ssim( hr_im(1:h,1:w), ori_im, 0, 0 );
-end
-if ch==3
-    hrim(:,:,1)  =  uint8(hr_im);
-    im_out       =  double(ycbcr2rgb( hrim ));
-else
-    im_out  =  hr_im;
-end
-return;
-
-
-
-
-
